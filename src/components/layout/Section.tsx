6d7248d2e306758f1d0e411010c5cc991a5a188a
방연지 <ktths1230@gmail.com>
2026-09-07 14:38:48 +0900

    Section.tsx 코드 추가

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,15 +1,20 @@
 import type { ReactNode } from 'react';
 import styles from './Section.module.css';
 
-type SectionProos = {
-    id : string;
-    title ?: string;
-    children?: ReactNode;
+type SectionProps = {
+  id: string;
+  title?: string;
+  children?: ReactNode;
 };
 
 /* 일반 섹션이 공통되게 적용될 수 있도록 지정 */
-export default function Section({ id, title, children }): SectionProps) {
-    <div className={styles.section}>
-    </div>
+export default function Section({ id, title, children }: SectionProps) {
+  return (
+    <section id={id} className={styles.section}>
+      <div className={styles.inner}>
+        {title && <h2 className={styles.title}>{title}</h2>}
+        {children}
+      </div>
+    </section>
+  );
 }
-
